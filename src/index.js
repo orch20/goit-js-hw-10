dc441215b8d15d5572489d0cff089347a1a58aef
orch20 <94862153+orch20@users.noreply.github.com>
2022-06-11 13:04:51 +0300

    fetch and search countries

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
+
+const refs = {
+  input: document.getElementById('search-box'),
+  list: document.querySelector('.country-list'),
+  info: document.querySelector('.country-info'),
+};
+
+refs.input.addEventListener('input', debounce(searchCountry, DEBOUNCE_DELAY));
+
+function searchCountry(e) {
+  let name = e.target.value;
+  fetchCountries(name)
+    .then(render)
+    .catch(error => console.log(error));
+}
+
+function fetchCountries(name) {
+  return fetch(`https://restcountries.com/v3.1/name/${name}`).then(response => {
+    return response.json();
+  });
+}
+
+function render(country) {
+  console.log(country);
+}
